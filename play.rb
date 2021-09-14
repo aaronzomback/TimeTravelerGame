a9150d8bc394d8eb7e2ccf3f2351568f425145af
aaronzomback <aaronzomback@gmail.com>
2021-09-14 22:33:46 +0200

    feat: initialize game build and engine in play.rb

--- play.rb
+++ play.rb
@@ -1,1 +1,15 @@
-# game build / initialization / engine run / goes here
+require './game_engine'
+require './time_traveler'
+
+
+# initialize game
+game = TimeTraveler.new
+
+# build the decision tree
+build_game = game.build
+
+# plug the decision tree into the game engine
+engine = GameEngine.new(build_game)
+
+# let 'er rip!
+engine.run
